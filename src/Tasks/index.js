e953bfa34ee792ab80a5fb6dbef663dfb124a522
Rafal Pietrzyk <rafipietrzyk@gmail.com>
2023-02-08 20:52:00 +0100

    hideDoneTasks renamed to hideDone

--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -1,29 +1,39 @@
 import "./style.css";
 
-const Tasks = ({ tasks, hideDoneTasks }) => (
-    <ul className="tasks">
-        {tasks.map(task => (
-            <li
-                key={task.id}
-                className={`tasks__item${task.done && hideDoneTasks
-                    ? "tasks__item--hidden"
-                    : ""}`
-                }
-            >
-                <button className="tasks__button tasks__button--toggleDone">
-                    {task.done ? "✓" : ""}
-                </button>
-                <span className={
-                    `tasks__content${task.done ? " tasks__content--done" : ""}`
-                }>
-                    {task.content}
-                </span>
-                <button className="tasks__button tasks__button--remove">
-                    🗑
-                </button>
-            </li>
-        ))}
-    </ul>
-);
+const onDelete = (name) => {
+console.log(`Nazwa zadania do usunięcia: ${name}`);
+}
+
+const Tasks = ({ tasks, hideDone }) => {
+    
+    return (
+        <ul className="tasks">
+            {tasks.map(task => (
+                <li
+                    key={task.id}
+                    className={`tasks__item${task.done && hideDone
+                        ? "tasks__item--hidden"
+                        : ""}`
+                    }
+                >
+                    <button className="tasks__button tasks__button--toggleDone">
+                        {task.done ? "✓" : ""}
+                    </button>
+                    <span className={
+                        `tasks__content${task.done ? " tasks__content--done" : ""}`
+                    }
+                    >
+                        {task.content}
+                    </span>
+                    <button className="tasks__button tasks__button--remove"
+                        onClick={() => onDelete(task.content)}
+                    >
+                        🗑
+                    </button>
+                </li>
+            ))}
+        </ul>
+    );
+}
 
 export default Tasks;
